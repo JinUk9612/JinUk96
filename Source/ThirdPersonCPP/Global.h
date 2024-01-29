928522bdca2cc2d4d4b3ff58cea907877436041d
JinUk <157696055+JinUk96@users.noreply.github.com>
2024-01-29 21:19:33 +0900

    0129

---
+++ Source/ThirdPersonCPP/Global.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "DrawDebugHelpers.h"
+
+#include "Kismet/GameplayStatics.h" 
+#include "Kismet/KismetSystemLibrary.h" // set timer 함수가 들어있는 라이브러리
+#include "Kismet/KismetMathLibrary.h" // cross or dot 함수가 들어있는 라이브러리
+
+#include "Utilities/CHelpers.h"
+#include "Utilities/CLog.h"
---
+++ Source/ThirdPersonCPP/ThirdPersonCPP.Build.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ThirdPersonCPP : ModuleRules
+{
+	public ThirdPersonCPP(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		PublicIncludePaths.Add(ModuleDirectory); //Addtional Include Path ¿Í °°À½ 
+	}
+}
